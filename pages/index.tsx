3925e1d60404cd89e0bffff507998f7fb00f67ba
Jared Henderson <jared@netrivet.com>
2020-10-22 12:47:51 -0400

    start jared tweaks from CNA

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Head from 'next/head'
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <Head>
+        <title>Akron Snowmen</title>
+      </Head>
+      <h1>Akron Snowmen</h1>
+    </>
+  )
+}
+
+export default Home;
