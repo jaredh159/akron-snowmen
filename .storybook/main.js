4e8f889e591505da046a4319b13676dc90c1d827
Jared Henderson <jared@netrivet.com>
2020-10-23 10:25:21 -0400

    tweak storybook setup for speed on pi

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,18 @@
 module.exports = {
   "stories": [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.tsx"
   ],
   "addons": [
     "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+    {
+      name: '@storybook/addon-essentials',
+      options: {
+        docs: false,
+      }
+    }
+  ],
+  typescript: {
+    check: false,
+    reactDocgen: false
+  }
 }
--- stories/Button.tsx
+++ stories/Button.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
-const Button: React.FC = () => <button>I am a button</button>
+const Button: React.FC = () => <button>I am a button and i am cool</button>
 
 export default Button;
+
+// @TODO -- check that css modules work in storybook
