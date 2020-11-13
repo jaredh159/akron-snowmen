6da6af1ee3c7468a76455f5bf42f2f3da4338b2b
Pep <willowfaithhenderson@gmail.com>
2020-11-13 15:40:48 +0000

    made a simple footer, whit links going to the friendsLibrary website.

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
 import styles from './Footer.module.css';
 
-const Footer: React.FC = () => {
+type Props = {
+linkOne: string;
+linkTwo: string;
+linkThree: string;
+linkFour: string;
+linkPageOne: string;
+linkPageTwo: string;
+linkPageThree: string;
+linkPageFour: string;
+}
+
+const Footer: React.FC<Props> = ({linkOne, linkTwo, linkThree, linkFour, linkPageOne, linkPageTwo, linkPageThree, linkPageFour}) => {
   return (
-    <div className={styles.aroundEverything}>
-    <div className={styles.mainDivForFooter}></div>
+    <div className={styles.mainDivForFooter}>
+      <a className="links" href={linkOne}>{linkPageOne}</a>
+      <a className="links" href={linkTwo}>{linkPageTwo}</a>
+      <a className="links" href={linkThree}>{linkPageThree}</a>
+      <a className="links" href={linkFour}>{linkPageFour}</a>
     </div>
   )
 };
--- stories/Footer.stories.tsx
+++ stories/Footer.stories.tsx
@@ -5,8 +5,11 @@ import Footer from '../components/Footer';
 const storiesMeta: Meta = {
   title: `Footer`,
   component: Footer,
+  parameters: {
+  layout: "fullscreen",
+  }
 };
 
 export default storiesMeta;
 
-export const Primary = () => <Footer />;
+export const Primary = () => <Footer linkPageOne="Home" linkPageTwo="Staff" linkPageThree="About Us" linkPageFour="What we do" linkOne={"https://friendslibrary.com"} linkTwo={"https://friendslibrary.com"} linkThree={"https://friendslibrary.com"} linkFour={"https://friendslibrary.com"}/>;
