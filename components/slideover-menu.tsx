78a6776071c503f47a9da682763bf18afcee624a
tabitha <beckfoote@gmail.com>
2020-11-16 15:57:43 +0000

    implemented changes that other people suggested

--- components/slideover-menu.tsx
+++ components/slideover-menu.tsx
@@ -6,7 +6,7 @@ const SlideoverMenu: React.FC = () => {
   <div className={styles.menu}>
       <div className={styles.notSureWhatThisIsYet}>the thing that closes this should go here</div>
       <div className={styles.flexContainer}>
-      <a className={styles.link} href="https://www.akronsnowmen.com">Home</a>
+      <a className={styles.link} id={styles.firstLink} href="https://www.akronsnowmen.com">Home</a>
       <a className={styles.link} href="https://www.akronsnowmen.com/about-akron-snowmen">About us</a>
       <a className={styles.link} href="https://www.akronsnowmen.com/akron-commercial-snow-removal-services">What we can do</a>
       <a className={styles.link} href="https://www.akronsnowmen.com/employment-akron-snowmen">Employment/subcontractor</a>
