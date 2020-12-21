9a4ad54e194e103fbbe72e4ac21711caa2bf0a9e
Ritty <harrietlovesgoats@gmail.com>
2020-12-21 21:30:29 +0000

    WIP: done

---
+++ stories/Nav.stories.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Meta } from "@storybook/react";
+import Nav from "../components/Nav";
+
+const storiesMeta: Meta = {
+  title: `Nav`,
+  component: Nav,
+};
+
+export default storiesMeta;
+
+export const Primary = () => <Nav />;
