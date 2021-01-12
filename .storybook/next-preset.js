bcd55667421cc4abd0af246841341f28780c609c
Jared Henderson <jared@netrivet.com>
2021-01-12 12:08:24 -0500

    skip using next/image in ImageAndTextBlock component (for now)

--- .storybook/next-preset.js
+++ .storybook/next-preset.js
@@ -61,18 +61,6 @@ module.exports = {
       ],
     });
 
-    newConfig.plugins.push(
-      new webpack.DefinePlugin({
-        'process.env.__NEXT_IMAGE_OPTS': JSON.stringify({
-          deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-          imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-          domains: [],
-          path: '/',
-          loader: 'default',
-        }),
-      }),
-    );
-
     return newConfig;
   },
 };
