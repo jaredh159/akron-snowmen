cca6f808ec90387f8acdf0de0a7a34d9f023b137
Jared Henderson <jared@netrivet.com>
2021-01-12 13:43:06 -0500

    basic homepage integration

--- components/Form.tsx
+++ components/Form.tsx
@@ -5,28 +5,26 @@ import styles from './Form.module.css';
 
 const Form: React.FC = () => {
   return (
-    <>
-      <form className={styles.form}>
-        <div className={cx(styles.inputs, styles.formDiv)}>
-          <div className={cx(styles.formDiv, styles.inputDivs)}>
-            <label>Name</label>
-            <input type="text" />
-          </div>
-
-          <div className={cx(styles.inputDivs, styles.email, styles.formDiv)}>
-            <label>Email</label>
-            <input type="email" />
-          </div>
+    <form className={styles.form}>
+      <div className={cx(styles.inputs, styles.formDiv)}>
+        <div className={cx(styles.formDiv, styles.inputDivs)}>
+          <label>Name</label>
+          <input type="text" />
         </div>
 
-        <div className={styles.formDiv}>
-          <label>Message</label>
-          <textarea rows={10}></textarea>
+        <div className={cx(styles.inputDivs, styles.email, styles.formDiv)}>
+          <label>Email</label>
+          <input type="email" />
         </div>
+      </div>
 
-        <Button>SEND MESSAGE</Button>
-      </form>
-    </>
+      <div className={styles.formDiv}>
+        <label>Message</label>
+        <textarea rows={10}></textarea>
+      </div>
+
+      <Button>SEND MESSAGE</Button>
+    </form>
   );
 };
 
--- components/HeroBlock.tsx
+++ components/HeroBlock.tsx
@@ -5,7 +5,7 @@ import Button from './Button';
 const HeroBlock: React.FC = () => {
   return (
     <div className={styles.entire}>
-      <img src="/plow.jpg" className={styles.image} alt="plow" />
+      <img src="./plow.jpg" className={styles.image} alt="plow" />
       <div className={styles.overlay}>
         <h1>Akron Snowmen</h1>
         <p>
