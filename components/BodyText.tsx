6fbdec23ac92675a1abb268b3bc681fe5907885a
winfield <winhenderson@gmail.com>
2021-02-01 19:06:03 +0000

    Fixed max-width and classnames thing

--- components/BodyText.tsx
+++ components/BodyText.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import styles from './BodyText.module.css';
+import cx from 'classnames';
 
 type Props = {
   className?: string;
 };
 
 const BodyText: React.FC<Props> = ({ className, children }) => {
-  return <div className={`${styles.main} ${className}`}>{children}</div>;
+  return <div className={cx(styles.main, className)}>{children}</div>;
 };
 
 export default BodyText;
