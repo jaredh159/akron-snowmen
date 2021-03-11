377cd38a6d2b47e2c335cab508a139e36564b473
kiah <miciahjohnhenderson@icloud.com>
2021-03-11 19:51:31 +0000

    finished link fix

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,20 +1,17 @@
-import React, { useState } from "react";
-import styles from "./Nav.module.css";
-import cx from "classnames";
+import React, { useState } from 'react';
+import styles from './Nav.module.css';
+import cx from 'classnames';
 
 const Nav: React.FC = () => {
   const [whoseActive, setWhoseActive] = useState<
-    "welcome" | "about" | "services" | "contact"
-  >("welcome");
+    'welcome' | 'about' | 'services' | 'contact'
+  >('welcome');
 
   return (
     <ul className={styles.outerNav}>
       <li
-        className={cx(
-          styles.link,
-          whoseActive === "welcome" ? styles.active : ""
-        )}
-        onClick={() => setWhoseActive("welcome")}
+        className={cx(styles.link, whoseActive === 'welcome' ? styles.active : '')}
+        onClick={() => setWhoseActive('welcome')}
       >
         <a href="#heroBlock" className={styles.aTag}>
           Welcome
@@ -22,22 +19,16 @@ const Nav: React.FC = () => {
       </li>
 
       <li
-        className={cx(
-          styles.link,
-          whoseActive === "about" ? styles.active : ""
-        )}
-        onClick={() => setWhoseActive("about")}
+        className={cx(styles.link, whoseActive === 'about' ? styles.active : '')}
+        onClick={() => setWhoseActive('about')}
       >
         <a href="#ImageBlock" className={styles.aTag}>
           About us
         </a>
       </li>
       <li
-        className={cx(
-          styles.link,
-          whoseActive === "services" ? styles.active : ""
-        )}
-        onClick={() => setWhoseActive("services")}
+        className={cx(styles.link, whoseActive === 'services' ? styles.active : '')}
+        onClick={() => setWhoseActive('services')}
       >
         <a href="#pinkBlock" className={styles.aTag}>
           Our Services
@@ -45,11 +36,8 @@ const Nav: React.FC = () => {
       </li>
 
       <li
-        className={cx(
-          styles.link,
-          whoseActive === "contact" ? styles.active : ""
-        )}
-        onClick={() => setWhoseActive("contact")}
+        className={cx(styles.link, whoseActive === 'contact' ? styles.active : '')}
+        onClick={() => setWhoseActive('contact')}
       >
         <a href="#contactBlock" className={styles.aTag}>
           Contact Us
