bcd61aa8325afbc466eddf71cd39acd44b2766d5
kiah <miciahjohnhenderson@icloud.com>
2021-03-11 19:48:36 +0000

    textarea resize and half of link fix lol

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,41 +1,59 @@
-import React, { useState } from 'react';
-import styles from './Nav.module.css';
-import cx from 'classnames';
+import React, { useState } from "react";
+import styles from "./Nav.module.css";
+import cx from "classnames";
 
 const Nav: React.FC = () => {
   const [whoseActive, setWhoseActive] = useState<
-    'welcome' | 'about' | 'services' | 'contact'
-  >('welcome');
+    "welcome" | "about" | "services" | "contact"
+  >("welcome");
 
   return (
     <ul className={styles.outerNav}>
       <li
-        className={cx(styles.link, whoseActive === 'welcome' ? styles.active : '')}
-        onClick={() => setWhoseActive('welcome')}
+        className={cx(
+          styles.link,
+          whoseActive === "welcome" ? styles.active : ""
+        )}
+        onClick={() => setWhoseActive("welcome")}
       >
-        <a href="#heroBlock">Welcome</a>
-        {/*I really can't think of anything else for the first link (^) */}
+        <a href="#heroBlock" className={styles.aTag}>
+          Welcome
+        </a>
       </li>
 
       <li
-        className={cx(styles.link, whoseActive === 'about' ? styles.active : '')}
-        onClick={() => setWhoseActive('about')}
+        className={cx(
+          styles.link,
+          whoseActive === "about" ? styles.active : ""
+        )}
+        onClick={() => setWhoseActive("about")}
       >
-        <a href="#ImageBlock">About us</a>
+        <a href="#ImageBlock" className={styles.aTag}>
+          About us
+        </a>
+      </li>
+      <li
+        className={cx(
+          styles.link,
+          whoseActive === "services" ? styles.active : ""
+        )}
+        onClick={() => setWhoseActive("services")}
+      >
+        <a href="#pinkBlock" className={styles.aTag}>
+          Our Services
+        </a>
       </li>
 
       <li
-        className={cx(styles.link, whoseActive === 'services' ? styles.active : '')}
-        onClick={() => setWhoseActive('services')}
+        className={cx(
+          styles.link,
+          whoseActive === "contact" ? styles.active : ""
+        )}
+        onClick={() => setWhoseActive("contact")}
       >
-        <a href="#pinkBlock">Our Services</a>
-      </li>
-
-      <li
-        className={cx(styles.link, whoseActive === 'contact' ? styles.active : '')}
-        onClick={() => setWhoseActive('contact')}
-      >
-        <a href="#contactBlock">Contact Us</a>
+        <a href="#contactBlock" className={styles.aTag}>
+          Contact Us
+        </a>
       </li>
     </ul>
   );
