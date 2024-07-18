a5af9d969dbe86364e6b80bd8d64c17d2ad58642
Tabitha <beckfoote@gmail.com>
2024-07-18 11:22:18 -0400

    update client list and hero picture

--- pages/clients.tsx
+++ pages/clients.tsx
@@ -1,24 +1,29 @@
-import React from 'react';
-import ArticlePage from '../components/ArticlePage';
-import BodyText from '../components/BodyText';
-import ArticleImage from '../components/ArticleImage';
-import Layout from '../components/Layout';
+import React from "react";
+import ArticlePage from "../components/ArticlePage";
+import BodyText from "../components/BodyText";
+import ArticleImage from "../components/ArticleImage";
+import Layout from "../components/Layout";
 
 const Article: React.FC = () => {
   return (
     <Layout>
-      <ArticlePage color="rgba(0, 0, 0, 0.7)" image="./snow.jpg" headline="Our Clients">
+      <ArticlePage
+        color="rgba(0, 0, 0, 0.7)"
+        image="./lights.jpg"
+        headline="Our Clients"
+      >
         <div className="max-w-4xl">
           <BodyText>
-            Akron Snowmen is best represented by our long-standing relationships with our
-            clients. We recognize the level of trust and quality of workmanship required
-            to maintain a good rapport with those we serve. If you’re looking for
-            confirmation that Akron Snowmen is a reputable company with outstanding
-            customer service, just ask them.
+            Akron Snowmen is best represented by our long-standing relationships
+            with our clients. We recognize the level of trust and quality of
+            workmanship required to maintain a good rapport with those we serve.
+            If you’re looking for confirmation that Akron Snowmen is a reputable
+            company with outstanding customer service, just ask them.
           </BodyText>
           <ArticleImage image="./logo-cloud-2.png" direction="right" />
           <BodyText>
-            Among the list of Akrons Snowmen’s current and former clients include:
+            Among the list of Akrons Snowmen’s current and former clients
+            include:
           </BodyText>
           <ul className="list-disc mb-6 pl-8 space-y-4">
             <li>
@@ -28,7 +33,12 @@ const Article: React.FC = () => {
               <span>Coventry Schools</span>
             </li>
             <li>
-              <span>Courtyard Marriot</span>
+              <span>Lowe's Home Improvement</span>
+            </li>
+            <li>
+              <span>
+                United States Postal Service (should we say which locations?)
+              </span>
             </li>
             <li>
               <span>Akron-Beacon Journal</span>
@@ -38,6 +48,9 @@ const Article: React.FC = () => {
             </li>
             <li>
               <span>Progressive Insurance Company (Akron Headquarters)</span>
+            </li>
+            <li>
+              <span>BJ's wholesale</span>
             </li>
             <li>
               <span>Shantz Cartage (Semi Truck Terminal)</span>
@@ -50,30 +63,23 @@ const Article: React.FC = () => {
             </li>
             <li>
               <span>
-                Associated Property Management (Condominiums throughout the Greater Akron
-                area)
+                Associated Property Management (Condominiums throughout the
+                Greater Akron area)
               </span>
-            </li>
-            <li>
-              <span>TH Martin (Akron Children’s Hospital Service Provider)</span>
             </li>
             <li>
               <span>
-                Pickard Commercial Group (Commercial Properties throughout the Greater
-                Akron area)
+                TH Martin (Akron Children’s Hospital Service Provider)
               </span>
+            </li>
+            <li>
+              <span>Citizens Bank</span>
             </li>
             <li>
               <span>Best Buy</span>
             </li>
             <li>
               <span>Barnes and Noble Bookstores</span>
-            </li>
-            <li>
-              <span>
-                Ferrandino and Son (National Construction and Facility Maintenance
-                Solutions Company)
-              </span>
             </li>
           </ul>
         </div>
