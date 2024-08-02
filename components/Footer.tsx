768ac8c8cb17db33e97ef5436431971029f6b1e0
Tabitha <beckfoote@gmail.com>
2024-08-02 16:35:17 -0400

    reset, the bad changes to the contact block are stashed now

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,22 +1,26 @@
-import React from 'react';
-import cx from 'classnames';
-import styles from './styles/Footer.module.css';
+import React from "react";
 
 const Footer: React.FC = () => {
   return (
-    <footer className={styles.footer}>
-      <ul className={styles.links}>
-        <li className={cx(styles.first, styles.items)}>
+    <footer className="h-[175px] bg-purple-footer w-full p-4 sm:h-[115px]">
+      <ul className="flex flex-col h-3/4 text-white/20 text-xs justify-evenly pt-10 sm:block sm:pt-[2.7rem]">
+        <li className="sm:inline-block sm:border-r sm:border-white/20 sm:pt-[5px] sm:pr-5 sm:pb-[0.2rem] sm:pl-[5px] md:pl-[0.9rem]">
           © {new Date().getFullYear()} — All rights reserved.
         </li>
-        <li className={cx(styles.second, styles.items)}>
-          <a className={styles.link1} href="https://www.virtuosolandscapes.com">
+        <li className="w-[118px] sm:inline-block sm:w-[10rem] sm:text-center sm:border-r sm:border-white/20 sm:py-[5px]">
+          <a
+            className="border-b border-b-white/30 border-dotted"
+            href="https://www.virtuosolandscapes.com"
+          >
             Virtuoso Landscaping
           </a>
         </li>
-        <li className={cx(styles.third, styles.items)}>
+        <li className="sm:inline-block sm:border-r sm:border-white/20 sm:pt-[5px] sm:pr-[20px] sm:pb-[5px] sm:pl-[20px]">
           Website:
-          <a className={styles.link2} href="https://homework.howtocomputer.link">
+          <a
+            className="border-b border-b-white/30 border-dotted w-5"
+            href="https://homework.howtocomputer.link"
+          >
             HTC
           </a>
         </li>
